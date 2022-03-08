2b6427726a5085c9182e5f3db0e4cba860698f67
Ryan Attarchi <attarchi@me.com>
2022-03-08 12:36:02 +0100

    Setup multiple Speedtest containers for different networks

--- speedtest/scripts/init_test_connection.sh
+++ speedtest/scripts/init_test_connection.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 FILE="/app/speedtest/test_connection.log"
 
+HOSTNAME_COMMAND=${HOSTNAME_COMMAND:=hostname}
+if ! HOSTVALUE=$($HOSTNAME_COMMAND 2>&1); then
+        HOSTVALUE=local
+fi
+
 while true 
 do 
     TIMESTAMP=$(date '+%s')
@@ -16,9 +21,9 @@ do
     UPLOAD=$(cat $FILE | grep "Upload:" | awk -F " " '{print $2}')
     PING=$(ping -qc1 google.com 2>&1 | awk -F'/' 'END {print (/^round-trip/? $4:"-100")}')
     echo "Download: $DOWNLOAD Upload: $UPLOAD Ping: $PING   $TIMESTAMP"
-    curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "download,host=local value=$DOWNLOAD"
-    curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "upload,host=local value=$UPLOAD"
-    curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "ping,host=local value=$PING"
+    curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "download,host=$HOSTVALUE value=$DOWNLOAD"
+    curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "upload,host=$HOSTVALUE value=$UPLOAD"
+    curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "ping,host=$HOSTVALUE value=$PING"
     sleep $TEST_INTERVAL
 
 done
